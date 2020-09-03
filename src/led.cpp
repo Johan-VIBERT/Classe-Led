5bb7d2b3ffd033329d2feb9b9ad662f144e5f1c7
Johan-VIBERT <70724120+Johan-VIBERT@users.noreply.github.com>
2020-09-03 22:46:02 +0200

    Add files via upload

---
+++ src/led.cpp
@@ -0,0 +1,70 @@
+/*! ===========================================================================================================================
+  Classe: Led.cpp                        Fichier d'impémentation
+  Editor: Lycée Marcel Callo
+  date:02-10-2017
+//============================================================================================================================*/
+
+
+/*! ===========================================================================================================================
+//                         Directives
+// ===========================================================================================================================*/
+#include <Arduino.h>
+#include <Led.h>
+
+
+Led::Led(byte broche)
+   /// le constructeur permet d'initialiser les attributs privés
+   /// et de faire le setup des pins Arduino
+{
+  _pin = broche;
+
+  pinMode(_pin, OUTPUT);
+  Eteindre(true);
+}
+
+
+void Led::Allumer(bool enable)
+{
+  if (enable)
+  {
+     _etat = HIGH; // Allumé
+     Rafraichir();
+  }
+
+
+
+}
+void Led::Eteindre(bool enable)
+{
+
+  if (enable)
+  {
+   _etat = LOW; // Eteint
+   Rafraichir();
+  }
+
+
+}
+
+void Led::Rafraichir()
+{
+  digitalWrite(_pin, _etat);
+}
+
+void Led:: Flash(bool enable,int temps,byte nb)
+{
+ if (enable)
+ {
+     for (char i=0;i<nb;i++)
+     {
+      Allumer(enable);
+      delay(temps);
+      Eteindre(enable);
+      delay(temps);
+     }
+ }
+
+
+
+
+}
